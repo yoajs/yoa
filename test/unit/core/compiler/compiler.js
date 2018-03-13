e8f823420ce339cb675a76661b67d74ab872f0e1
moonrailgun <moonrailgun@gmail.com>
2018-03-13 17:11:42 +0800

    模板测试

--- test/unit/core/compiler/compiler.js
+++ test/unit/core/compiler/compiler.js
@@ -1,10 +1,4 @@
 describe('Compiler', function() {
-  it("should compile direct", function() {
-    var render = new Yoa().compile(' ');
-    console.log('render', render);
-    // TODO
-  })
-
   it("should compile mustaches", function() {
     var el = createTestElement("compilerMustache", '{{msg}}');
     var app = new Yoa({
@@ -14,5 +8,61 @@ describe('Compiler', function() {
       }
     });
     expect(el.innerHTML).to.equal("Hello Yoa!!!");
+
+    var el2 = createTestElement("compilerMustache2", '{{  msg}}');
+    var app = new Yoa({
+      el: "#compilerMustache2",
+      data: {
+        msg: "Hello Yoa!!!"
+      }
+    });
+    expect(el.innerHTML).to.equal("Hello Yoa!!!");
+
+    var el3 = createTestElement("compilerMustache3", '{{msg   }}');
+    var app = new Yoa({
+      el: "#compilerMustache3",
+      data: {
+        msg: "Hello Yoa!!!"
+      }
+    });
+    expect(el.innerHTML).to.equal("Hello Yoa!!!");
+
+    var el4 = createTestElement("compilerMustache4", '{{   msg   }}');
+    var app = new Yoa({
+      el: "#compilerMustache4",
+      data: {
+        msg: "Hello Yoa!!!"
+      }
+    });
+    expect(el.innerHTML).to.equal("Hello Yoa!!!");
   });
+
+  it("should compile mustaches with whitespace", function() {
+    var el = createTestElement("compilerMustacheWithWhitespace", '  {{msg}}');
+    var app = new Yoa({
+      el: "#compilerMustacheWithWhitespace",
+      data: {
+        msg: "Hello Yoa!!!"
+      }
+    });
+    expect(el.innerHTML).to.equal("  Hello Yoa!!!");
+
+    var el = createTestElement("compilerMustacheWithWhitespace2", '  {{msg}}  ');
+    var app2 = new Yoa({
+      el: "#compilerMustacheWithWhitespace2",
+      data: {
+        msg: "Hello Yoa!!!"
+      }
+    });
+    expect(el.innerHTML).to.equal("  Hello Yoa!!!  ");
+
+    var el = createTestElement("compilerMustacheWithWhitespace3", '{{msg}}  ');
+    var app3 = new Yoa({
+      el: "#compilerMustacheWithWhitespace3",
+      data: {
+        msg: "Hello Yoa!!!"
+      }
+    });
+    expect(el.innerHTML).to.equal("Hello Yoa!!!  ");
+  })
 })
